7be0edee23c73f43ba9d5cb801b3f0000726dd7b
Ala Rai <spensa2010alarai@gmail.com>
2018-10-13 13:52:57 +0700

    Perubahan home

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,12 @@
-<h1>Ini Adalah Home</h1>
+
+<div class="jumbotron">
+    <div class="container">
+        <h1>Welcome to my site</h1>
+        <p>Follow my social media</p>
+        <p>
+            <a class="btn btn-primary btn-lg">Facebook</a>
+            <a class="btn btn-default btn-lg">Instagram</a>
+            <a class="btn btn-primary btn-lg">Twitter</a>
+        </p>
+    </div>
+</div>
