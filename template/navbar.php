8ad784549c14fdd05808737f46dab9623e86710f
Ala Rai <spensa2010alarai@gmail.com>
2018-10-13 13:35:32 +0700

    First commit

---
+++ template/navbar.php
@@ -0,0 +1,11 @@
+<div class="navbar navbar-default">
+    <a class="navbar-brand" href="#">Title</a>
+    <ul class="nav navbar-nav">
+        <li class="active">
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="index.php?page=about">About</a>
+        </li>
+    </ul>
+</div>
